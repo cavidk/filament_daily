45da3f55f40ff53769526b678061b4ccdce8ba88
cavidkazimzada <cavid.kazimov.98@bk.ru>
2024-07-24 01:24:45 +0400

    seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
 use App\Models\Product;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
@@ -21,6 +22,10 @@ class DatabaseSeeder extends Seeder
 //            'email' => 'test@example.com',
 //        ]);
 
-        Product::factory()->count(3)->create();
+//        Product::factory()->count(4)->create();
+
+        Product::factory()->count(4)->create();
+        Category::factory()->count(4)->create();
+
     }
 }
