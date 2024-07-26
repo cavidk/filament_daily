5ff0b9f3ebffacc08d8ffd8b229614d8e6bb9470
cavidkazimzada <cavid.kazimov.98@bk.ru>
2024-07-26 17:48:32 +0400

    created Category field&relations

---
+++ app/Filament/Resources/CategoryResource/Pages/EditCategory.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Filament\Resources\CategoryResource\Pages;
+
+use App\Filament\Resources\CategoryResource;
+use Filament\Actions;
+use Filament\Forms\Components\TextInput;
+use Filament\Resources\Pages\EditRecord;
+
+class EditCategory extends EditRecord
+{
+    protected static string $resource = CategoryResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\DeleteAction::make(),
+        ];
+    }
+}
